3e1ae6accd5652bbe2fbe8685370bd6fe9234fb5
LordFlixXx <159223630+LordFlixXx@users.noreply.github.com>
2024-12-04 03:06:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function getQueryStringParameter(name) {
 }
 
 // Variáveis principais
-const movieId = getQueryStringParameter("hash");
-const imdbId = getQueryStringParameter("id");
-const defaultPoster = "https://i.imgur.com/IG0AIMf.jpeg";
+const movieId = getQueryStringParameter("id");
+const imdbId = getQueryStringParameter("imdb");
+const defaultPoster = "https://lh3.googleusercontent.com/d/1IP-lAqFygjiMHsOJ7dHZ11-hJc9_9Nhj?authuser=0";
 
 // Validação do ID do filme
 if (!movieId) {
