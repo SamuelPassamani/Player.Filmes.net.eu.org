ff25bfca72cb6f9c61428eae640f2413824f76b3
Samuel Teixeira Passamani <159223630+LordFlixXx@users.noreply.github.com>
2025-01-24 13:30:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const puppeteer = require('puppeteer');
+const chromium = require("chrome-aws-lambda"); // Adicionando chrome-aws-lambda
+const fetch = require("node-fetch");
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event) {
   const imdb = event.queryStringParameters.imdb;
 
   if (!imdb) {
@@ -10,19 +11,45 @@ exports.handler = async function(event, context) {
     };
   }
 
+  // Busca o hash do torrent usando a API YTS
+  async function fetchTorrentHash(imdbId) {
+    const apiUrl = `https://yts.mx/api/v2/movie_details.json?imdb_id=${imdbId}`;
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      if (data.status === "ok" && data.data.movie.torrents.length > 0) {
+        return data.data.movie.torrents[0].hash;
+      } else {
+        throw new Error("Torrent hash not found.");
+      }
+    } catch (error) {
+      console.error("Failed to fetch torrent hash:", error);
+      throw error;
+    }
+  }
+
   let browser;
   try {
-    // Lançar o Puppeteer em ambiente headless
-    browser = await puppeteer.launch({ headless: true });
+    // Obter o hash do torrent
+    const torrentHash = await fetchTorrentHash(imdb);
+
+    // Lançar o Puppeteer em ambiente serverless
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
     const page = await browser.newPage();
 
-    // Navegar para o site com o hash do torrent
-    const url = `https://webtor.io/${imdb}`;
+    // Navegar para o site WebTor com o hash do torrent
+    const url = `https://webtor.io/${torrentHash}`;
     await page.goto(url, { waitUntil: "domcontentloaded" });
 
-    // Pegar o link de download do torrent
+    // Extrair o link de download do torrent
     const downloadLink = await page.$eval(
-      "a.btn.btn-sm.btn-accent.m-2.closeable-close",
+      'a[data-download="true"]', // Seletor atualizado
       (link) => link.getAttribute("href")
     );
 
@@ -31,9 +58,10 @@ exports.handler = async function(event, context) {
       body: JSON.stringify({ downloadLink }),
     };
   } catch (error) {
+    console.error("Error:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Failed to fetch the download link" }),
+      body: JSON.stringify({ error: "Failed to fetch the download link." }),
     };
   } finally {
     if (browser) {
